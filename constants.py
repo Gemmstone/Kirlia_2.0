4d7ca9e63c8dc8ee66668933182f2ca0437699fb
gemmstone <emmaleduartec@gmail.com>
2022-10-02 17:35:31 -0600

    .gitignore is now working

--- constants.py
+++
@@ -1,6 +0,0 @@
-DISCORD_TOKEN = "NzcwMTI1NjUxNDk0NTY3OTY2.GuIteZ.pQ_dx7qIOQFpJHF72D630Pvbcor37tBTbGe3LI"
-IMGUR_TOKEN = {
-    "client_id": "aeef3ead2dc56ac",
-    "client_secret": "78f9763e2bf60889920fd87dc7d6e0e51e8e9a6f"
-}
-DEEPAI_KEY = 'ce7d8fc1-8aa1-4986-9e24-9534cd6412b0'
